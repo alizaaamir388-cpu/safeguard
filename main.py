10aa43bdf308463d6813fe0c6906604ffc5b44ba
Aliza <alizaaamir388@gmail.com>
2026-05-15 13:41:29 +0500

    fix port

--- main.py
+++ main.py
@@ -215,3 +215,6 @@ def check_content(data: CheckContent):
 @app.get("/")
 def root():
     return {"message": "SafeGuard AI Parental Control System is running!"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
